5c8587823068b31f29fec47949c5292a22ac541f
Luciano Lima <luscious.ds@gmail.com>
2018-02-24 03:01:22 -0300

    Small character.service alteration to have an example of how to make custom routes.

--- src/services/characters/characters.service.js
+++ src/services/characters/characters.service.js
@@ -11,6 +11,31 @@ module.exports = function () {
   // Initialize our service with any options it requires
   app.use('/characters', createService(options));
 
+  // Get an array from character.
+  // Just for user case, if necessary to do something route specific.
+  app.use('/characters/:characterId/:arrayName/:arrayId?', {
+    find(params) {
+      const { route: { characterId, arrayName, arrayId } } = params;
+      const service = app.service('characters');
+      return service.get(characterId, params).then(character => {
+        const array = character[arrayName];
+        return arrayId ? array.find(element => element._id === arrayId) : array;
+      });
+    },
+    // Delete specific element from character
+    remove(id, params) {
+      const { route: { characterId, arrayName, arrayId } } = params;
+      if (!characterId || !arrayName || !arrayId) throw Error('Invalid.');
+
+      const service = app.service('characters');
+      return service.get(characterId, params).then(character => {
+        const element = character[arrayName].find(el => el._id === arrayId);        
+        element.deleted = true;
+        return service.update(character._id, character, params);
+      });
+    }
+  });
+
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('characters');
 
